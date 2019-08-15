ca386db6d2262bc490170472d72b5ce20de2ffc7
David Sanders <dsanderscanada@gmail.com>
2019-08-14 20:23:31 -0400

    Unit test changes

--- tests/test_App.py
+++ tests/test_App.py
@@ -17,12 +17,28 @@ class TestApp(TestCase):
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
         self.app = app.test_client()
+        self.default_modes_url = self.app.application.config['cowbull_modes_url']
 
     def tearDown(self):
         pass
 
     # Expect the game to fail in unit test. Cowbull server should NOT be running
     def test_no_game(self):
+        self.app.application.config['cowbull_modes_url'] = self.default_modes_url
         response = self.app.get('/', follow_redirects=True)
         response_expected = "Game is unavailable"
         self.assertTrue(response_expected in str(response.get_data()))
+
+    def test_health_notready(self):
+        self.app.application.config['cowbull_modes_url'] = self.default_modes_url
+        response = self.app.get('/health', follow_redirects=True)
+        response_expected = "Game is unavailable"
+        self.assertTrue(response_expected in str(response.get_data()))
+
+    def test_bad_cowbull_url(self):
+        with self.assertRaises(ValueError):
+            self.app.application.config['cowbull_modes_url'] = None
+            self.app.get('/', follow_redirects=True)
+        self.app.application.config['cowbull_modes_url'] = self.default_modes_url
+
+    
